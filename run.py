a346b9adddb24e59a42e9a6a857beba938385db1
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2018-10-09 15:50:37 +0200

    basic loss mechanics

--- run.py
+++ run.py
@@ -45,17 +45,30 @@ for _ in range(NUM_BATCHES):
     # TODO: get_context method...
     context = [random.sample(OBJS, CONTEXT_SIZE) for _ in range(BATCH_SIZE)]
     target = [random.randrange(CONTEXT_SIZE) for _ in range(BATCH_SIZE)]
+    target_one_hot = tf.one_hot(target, depth=CONTEXT_SIZE)
     # TODO: make this work with length-2 signals
     message_logits = sender(
-        tf.concat([context, tf.one_hot(target, depth=CONTEXT_SIZE)], axis=1))
+        tf.concat([context, target_one_hot], axis=1))
     message = tf.squeeze(tf.one_hot(tf.multinomial(message_logits, num_samples=1),
                          depth=CONTEXT_SIZE))
     print(message_logits)
     print(message)
     choice_logits = receiver(tf.concat([context, message], axis=1))
     print(choice_logits)
-    choice = tf.squeeze(tf.multinomial(choice_logits, num_samples=1))
+    choice = tf.squeeze(tf.one_hot(tf.multinomial(choice_logits, num_samples=1),
+                                   depth=CONTEXT_SIZE))
     print(target)
     print(choice)
-    reward = tf.to_int64(tf.equal(target, choice))
+    reward = tf.to_float(tf.equal(target, tf.argmax(choice, axis=1)))
+    # reward 1/0 goes to 1/-1
+    advantages = 2*reward - 1
     print(reward)
+    print(advantages)
+
+    sender_loss = advantages * tf.nn.softmax_cross_entropy_with_logits_v2(
+        labels=message,
+        logits=message_logits)
+
+    receiver_loss = advantages * tf.nn.softmax_cross_entropy_with_logits_v2(
+        labels=choice,
+        logits=choice_logits)
